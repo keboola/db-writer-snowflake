8c87ea2268e0a89dd69cf530ae42c84390fbc6ec
Erik Zigo <info@spoluprace.net>
2017-03-29 11:38:40 +0200

    deploy update - deploy also with latest tag

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag dbwritersnowflake_app quay.io/keboola/db-writer-snowflake:$TRAVIS_TAG
 docker push quay.io/keboola/db-writer-snowflake:$TRAVIS_TAG
+docker push quay.io/keboola/db-writer-snowflake:latest
