2640b675da5e91d8f56d2107ebc75f35f74271ee
Erik Zigo <erik.zigo@keboola.com>
2018-09-08 13:15:09 +0200

    update snowflake drivers to 2.16.6

--- driver/downloadDriver.php
+++ driver/downloadDriver.php
@@ -27,6 +27,6 @@ $client =  new \Aws\S3\S3Client([
 
 $client->getObject([
     'Bucket' => 'keboola-configs',
-    'Key' => 'drivers/snowflake/snowflake_linux_x8664_odbc.2.12.88.tgz',
+    'Key' => 'drivers/snowflake/snowflake_linux_x8664_odbc.2.16.6.tgz',
     'SaveAs' => './snowflake_linux_x8664_odbc.tgz'
 ]);
