5bccc307dbfe62d9e73614cfefd5af28eee7fae8
Erik Zigo <erik.zigo@keboola.com>
2018-09-11 16:54:36 +0200

    tests: fixed testDefaultWarehouse

--- tests/Keboola/DbWriter/Writer/SnowflakeTest.php
+++ tests/Keboola/DbWriter/Writer/SnowflakeTest.php
@@ -241,23 +241,22 @@ class SnowflakeTest extends BaseTest
         // run without warehouse param
         unset($config['parameters']['db']['warehouse']);
 
-        /** @var Snowflake $writer */
-        $writer = $this->getWriter($config['parameters']);
-
-        $tables = $config['parameters']['tables'];
-        foreach ($tables as $table) {
-            $writer->drop($table['dbName']);
-        }
-        $table = $tables[0];
-
-        $s3Manifest = $this->loadDataToS3($table['tableId']);
-
         try {
-            $writer->create($table);
-            $writer->writeFromS3($s3Manifest, $table);
-            $this->fail('Run writer without warehouse should fail');
+            $this->getWriter($config['parameters']);
+            $this->fail('Create writer without warehouse should fail');
         } catch (UserException $e) {
             $this->assertRegExp('/Snowflake user has any \"DEFAULT_WAREHOUSE\" specified/ui', $e->getMessage());
+        }
+
+        // bad warehouse
+        $config = $this->config;
+        $config['parameters']['db']['warehouse'] = uniqid('test');
+
+        try {
+            $this->getWriter($config['parameters']);
+            $this->fail('Create writer wit invalid warehouse should fail');
+        } catch (UserException $e) {
+            $this->assertRegExp('/Invalid warehouse/ui', $e->getMessage());
         }
 
         // run with warehouse param
@@ -265,6 +264,11 @@ class SnowflakeTest extends BaseTest
 
         /** @var Snowflake $writer */
         $writer = $this->getWriter($config['parameters']);
+
+        $tables = $config['parameters']['tables'];
+        $table = $tables[0];
+
+        $s3Manifest = $this->loadDataToS3($table['tableId']);
 
         $writer->create($table);
         $writer->writeFromS3($s3Manifest, $table);
@@ -276,51 +280,6 @@ class SnowflakeTest extends BaseTest
             $conn->quoteIdentifier($warehouse)
         );
         $conn->query($sql);
-    }
-
-    public function testCredentialsDefaultWarehouse()
-    {
-        $config = $this->config;
-        $config['action'] = 'testConnection';
-        unset($config['parameters']['tables']);
-
-        $user = $config['parameters']['db']['user'];
-        $warehouse = $config['parameters']['db']['warehouse'];
-
-        // empty default warehouse, specified in config
-        $this->setUserDefaultWarehouse($user, null);
-
-        /** @var Snowflake $writer */
-        $writer = $this->getWriter($config['parameters']);
-        $writer->testConnection();
-
-        // empty default warehouse and not specified in config
-        unset($config['parameters']['db']['warehouse']);
-
-        /** @var Snowflake $writer */
-        $writer = $this->getWriter($config['parameters']);
-
-        try {
-            $writer->testConnection();
-            $this->fail('Test connection without warehouse and default warehouse should fail');
-        } catch (UserException $e) {
-            $this->assertRegExp('/Specify \"warehouse\" parameter/ui', $e->getMessage());
-        }
-
-        // bad warehouse
-        $config['parameters']['db']['warehouse'] = uniqid('test');
-
-        /** @var Snowflake $writer */
-        $writer = $this->getWriter($config['parameters']);
-
-        try {
-            $writer->testConnection();
-            $this->fail('Test connection with invalid warehouse ID should fail');
-        } catch (UserException $e) {
-            $this->assertRegExp('/Invalid warehouse/ui', $e->getMessage());
-        }
-
-        $this->setUserDefaultWarehouse($user, $warehouse);
     }
 
     public function testInvalidWarehouse(): void
