3cea0bab90d38c004a2961a31837c1bf19592796
Erik Zigo <erik.zigo@keboola.com>
2018-10-29 16:03:27 +0100

    Application::writeFullFromS3 with transaction

--- src/Keboola/DbWriter/Snowflake/Application.php
+++ src/Keboola/DbWriter/Snowflake/Application.php
@@ -89,9 +89,13 @@ class Application extends BaseApplication
         /** @var Snowflake $writer */
         $writer = $this['writer'];
 
+        $writer->beginTransaction();
+
         $writer->drop($tableConfig['dbName']);
         $writer->create($tableConfig);
         $writer->writeFromS3($s3info, $tableConfig);
+
+        $writer->commitTransaction();
     }
 
     private function getManifest($tableId)
