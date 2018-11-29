321003ce4b0e3e13cc75b59ec20a78fd5bf94169
Erik Zigo <erik.zigo@keboola.com>
2018-11-29 14:33:57 +0100

    Writer\Snowflake::getColumnsSqlDefinition() uses DataType\Definition::getSQLDefinition()

--- src/Keboola/DbWriter/Writer/Snowflake.php
+++ src/Keboola/DbWriter/Writer/Snowflake.php
@@ -586,29 +586,12 @@ class Snowflake extends Writer implements WriterInterface
         $sql = '';
 
         foreach ($columns as $col) {
-            $type = strtoupper($col['type']);
-            if (!empty($col['size']) && in_array(strtolower($col['type']), self::$typesWithSize)) {
-                $type .= sprintf("(%s)", $col['size']);
-            }
-            $null = $col['nullable'] ? 'NULL' : 'NOT NULL';
-
             $colDefinition = DataType\Definition::createFromTableMapping($col);
 
-            $default = '';
-            if ($colDefinition->getDefault() !== null) {
-                if ($colDefinition->getBasetype() === 'STRING') {
-                    $default = sprintf("DEFAULT %s", $this->db->quote($colDefinition->getDefault()));
-                } else {
-                    $default = sprintf("DEFAULT %s", addslashes($colDefinition->getDefault()));
-                }
-            }
-
             $sql .= sprintf(
-                "%s %s %s %s,",
+                "%s %s,",
                 $this->db->quoteIdentifier($col['dbName']),
-                $type,
-                $null,
-                $default
+                $colDefinition->getSQLDefinition()
             );
         }
 
