1e400bc1bafa1e57b2888d35d67b31e1cef77dee
Michal Jurečko <michal.jurecko@gmail.com>
2021-03-25 14:45:49 +0100

    ABS storage: load files from manifest

--- src/Keboola/DbWriter/Adapter/AbsAdapter.php
+++ src/Keboola/DbWriter/Adapter/AbsAdapter.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Keboola\DbWriter\Adapter;
 
+use Keboola\DbWriter\Exception\UserException;
 use Keboola\DbWriter\Writer\Snowflake;
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Common\Internal\Resources;
 
 class AbsAdapter implements IAdapter
 {
@@ -63,42 +67,82 @@ class AbsAdapter implements IAdapter
         );
     }
 
-    public function generateCopyCommand(string $tableName, string $stageName, array $columns): string
+    public function generateCopyCommands(string $tableName, string $stageName, array $columns): iterable
     {
-        $columnNames = array_map(function ($column) {
-            return Snowflake::quoteIdentifier($column['dbName']);
-        }, $columns);
+        $filesToImport = $this->getManifestEntries();
+        foreach (array_chunk($filesToImport, self::SLICED_FILES_CHUNK_SIZE) as $files) {
+            $quotedFiles = array_map(
+                function ($entry) {
+                    return Snowflake::quote(
+                        strtr($entry, [$this->getContainerUrl() => ''])
+                    );
+                },
+                $files
+            );
 
-        $transformationColumns = array_map(
-            function ($column, $index) {
-                if (!empty($column['nullable'])) {
-                    return sprintf("IFF(t.$%d = '', null, t.$%d)", $index + 1, $index + 1);
-                }
-                return sprintf('t.$%d', $index + 1);
-            },
-            $columns,
-            array_keys($columns)
-        );
+            yield sprintf(
+                'COPY INTO %s(%s) 
+            FROM (SELECT %s FROM %s t)
+            FILES = (%s)',
+                $tableName,
+                implode(', ', SqlHelper::getQuotedColumnsNames($columns)),
+                implode(', ', SqlHelper::getColumnsTransformation($columns)),
+                Snowflake::quote('@' . Snowflake::quoteIdentifier($stageName) . '/'),
+                implode(',', $quotedFiles)
+            );
+        }
+    }
 
-        $path = $this->name;
-        $pattern = '';
-        if ($this->isSliced) {
-            // key ends with manifest
-            if (strrpos($this->name, 'manifest') === strlen($this->name) - strlen('manifest')) {
-                $path = substr($this->name, 0, strlen($this->name) - strlen('manifest'));
-                $pattern = 'PATTERN="^.*(?<!manifest)$"';
+    private function getManifestEntries(): array
+    {
+        $blobClient = $this->getClient();
+        if (!$this->isSliced) {
+            // this is temporary solution copy into is not failing when blob not exists
+            try {
+                $blobClient->getBlob($this->container, $this->name);
+            } catch (ServiceException $e) {
+                throw new UserException('Load error: ' . $e->getErrorText(), 0, $e);
             }
+
+            [$this->getContainerUrl() . $this->name];
         }
 
-        return sprintf(
-            'COPY INTO %s(%s) 
-            FROM (SELECT %s FROM %s t)
-            %s',
-            $tableName,
-            implode(', ', $columnNames),
-            implode(', ', $transformationColumns),
-            Snowflake::quote('@' . Snowflake::quoteIdentifier($stageName) . '/' . $path),
-            $pattern
+        try {
+            $manifestBlob = $blobClient->getBlob($this->container, $this->name);
+        } catch (ServiceException $e) {
+            throw new UserException('Load error: manifest file was not found.', 0, $e);
+        }
+
+        $manifest = \GuzzleHttp\json_decode((string) stream_get_contents($manifestBlob->getContentStream()), true);
+        return array_map(function (array $entry) use ($blobClient) {
+            // this is temporary solution copy into is not failing when blob not exists
+            try {
+                /** @var string[] $parts */
+                $parts = explode(sprintf('blob.core.windows.net/%s/', $this->container), $entry['url']);
+                $blobPath = $parts[1];
+                $blobClient->getBlob($this->container, $blobPath);
+            } catch (ServiceException $e) {
+                throw new UserException('Load error: ' . $e->getErrorText(), 0, $e);
+            }
+            return str_replace('azure://', 'https://', $entry['url']);
+        }, $manifest['entries']);
+    }
+
+    private function getContainerUrl(): string
+    {
+        return sprintf('https://%s/%s', $this->connectionEndpoint, $this->container);
+    }
+
+    private function getClient(): BlobRestProxy
+    {
+        $sasConnectionString = sprintf(
+            '%s=https://%s;%s=%s',
+            Resources::BLOB_ENDPOINT_NAME,
+            $this->connectionEndpoint,
+            Resources::SAS_TOKEN_NAME,
+            $this->connectionAccessSignature
         );
+
+        return BlobRestProxy::createBlobService($sasConnectionString);
     }
 }
